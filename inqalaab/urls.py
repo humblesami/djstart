f58898e0a5840b82131be281fbdb0587b197a174
sami92news <sami.akram@92newshd.tv>
2022-11-06 16:38:31 +0500

    added new app

--- inqalaab/urls.py
+++ inqalaab/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 from .views import IndexClass, index_fun
 
 urlpatterns = [
+    path('', index_fun),
     path('class', IndexClass.as_view()),
-    path('fun', index_fun),
 ]

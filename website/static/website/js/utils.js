32d390b1c55b64ecf5b1fe1aadad28cf7bda6b02
sami92news <sami.akram@92newshd.tv>
2023-05-11 16:13:56 +0500

    load external script xhr

--- website/static/website/js/utils.js
+++ website/static/website/js/utils.js
@@ -9,5 +9,32 @@ var json_utils = {
 
     add_delete_url_param: function(){
 
+    },
+    loadExternalResource: function (resourceUrl, elem, time_out=5000){
+        var fetchTimer = undefined;
+        function completeResourceFetch(reason, html) {
+            console.log('Completed with ' + reason);
+            elem.innerHTML = html || '';
+            clearTimeout(fetchTimer);
+        }
+        function loadViaXhr(resourceUrl, onLoad, onError){
+            var xhr = new XMLHttpRequest();
+            fetchTimer = setTimeout(function() {
+                onError('Time out');
+                xhr.abort();
+            }, time_out);
+            try {
+                xhr.onload = function(resp) {
+                    var xmlString = resp.srcElement.responseText;
+                    onLoad('Done', xmlString);
+                };
+                xhr.open("GET", resourceUrl, true);
+                xhr.send();
+            } catch (e) {
+                onError(e);
+            }
+        }
+        loadViaXhr(resourceUrl,completeResourceFetch, completeResourceFetch);
+        console.log('Started');
     }
 }
